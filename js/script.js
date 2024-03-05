aa70bd9412d4e69cfa24a9e37c82da63aeffdb9b
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-03-05 19:24:05 +0000

    added the js code to the first time and add hello world button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: Mar 5 2024 
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
